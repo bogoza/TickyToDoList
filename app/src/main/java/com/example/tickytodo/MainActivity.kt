b9b755869a2d9458499d7a3fc5b24270a39e72b2
Giorgi Chachanidze <72442468+bogoza@users.noreply.github.com>
2022-11-05 23:10:14 +0400

    newOne

---
+++ app/src/main/java/com/example/tickytodo/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.tickytodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import com.example.tickytodo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //comment
+
+
+    }
+}
